a17f7f83ef2a30f95ca2c0cdee1ac13dba80699f
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2022-08-02 14:54:25 +0530

    Add files via upload

---
+++ WelcomeController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping(value="/welcome",method = RequestMethod.GET)
+	public String display()
+	{
+		return "success";
+	}
+
+}
+//http://localhost:8080/SpringWebMaven/welcome
